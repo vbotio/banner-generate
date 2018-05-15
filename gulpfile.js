572376fc4c338ee3153b455abacf987ec65144af
Vitor Boccio <vitorboccio@gmail.com>
2018-05-15 17:04:18 -0300

    including controllers and basic html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
+var sass = require('gulp-sass');
 
 gulp.task("browser-sync", function() {
     browserSync.init({
@@ -7,6 +8,16 @@ gulp.task("browser-sync", function() {
             baseDir: './'
         }
     });
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./css/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./css/*.scss', ['sass']);
 });
 
 gulp.task('default', ['browser-sync']);
