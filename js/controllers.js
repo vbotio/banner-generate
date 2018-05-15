d69264867065844427d1cc28d3a0349acc5c1ee2
Vitor Boccio <vitorboccio@gmail.com>
2018-05-15 20:43:41 -0300

    adding form

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,12 @@
 var app = angular.module("app", []);
 
-app.controller('mainCtrl', function(){
+app.controller('mainCtrl', function($scope){
+	$scope.generateBanner = generateBanner;
+	function generateBanner() {
+		domtoimage.toBlob(document.getElementById('wrapper'))
+	    .then(function(blob) {
+	      console.log("blob", blob)
+	      window.saveAs(blob, 'my-node.png');
+	    });
+	}
 })
